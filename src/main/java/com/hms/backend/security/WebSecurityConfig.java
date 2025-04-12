399f2cc4805e682507f39c58e0bdb6252d8d7527
Sushrut22 <sushlachure22@gmail.com>
2025-04-13 02:54:15 +0530

    Fix CORS

--- src/main/java/com/hms/backend/security/WebSecurityConfig.java
+++ src/main/java/com/hms/backend/security/WebSecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -89,6 +90,8 @@ public class WebSecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         // Configure CSRF protection, exception handling, session management, and authorization
         http.csrf(AbstractHttpConfigurer::disable) // Disable CSRF protection
+                // Enable CORS first
+                .cors(Customizer.withDefaults()) // Add this line
                 .exceptionHandling(exception ->
                         exception.authenticationEntryPoint(unauthorizedHandler))
                 // Set unauthorized handler
